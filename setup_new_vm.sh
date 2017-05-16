48c2fb4671f3ed2abb099b3a1b143a999a3f65bb
Kevin Stone <kevinleestone@gmail.com>
2017-05-16 22:37:16 +0000

    Added dotfiles

--- setup_new_vm.sh
+++ setup_new_vm.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Setup system dependencies and tools
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y python-virtualenv tmux
 
+# Setup dotfiles
+rm -f ~/.gitconfig
+ln -s $(readlink -f dotfiles/gitconfig) ~/.gitconfig
+
+# Setup python virtual environment
+rm -rf ~/env/py3
 virtualenv -p $(which python3) ~/env/py3
 source ~/env/py3/bin/activate
 pip install --upgrade pip
